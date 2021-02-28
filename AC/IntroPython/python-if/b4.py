ed558050d7749ecb6af52d86f7cf6341307255bb
Samir <samir.saravia.10@gmail.com>
2021-02-28 15:52:54 -0300

    tipos de valores

---
+++ AC/IntroPython/python-if/b4.py
@@ -0,0 +1,9 @@
+# tipos de valores
+print(type('Ola mundo'))
+print(type(7))
+
+print(type(True))  # bool sempre maiuscula as primera letra
+print(type(False))
+
+print(type('True'))  # pegadinha
+print(type('False'))
