063c0008dfd533f19e560014bb2154c111d1c04a
Samir <samir.saravia.10@gmail.com>
2021-02-28 15:48:14 -0300

    concatenar valores

---
+++ AC/IntroPython/primeiro-prog/a2_numeros.py
@@ -0,0 +1,6 @@
+print('primeiro numero: ')
+primeiro_num = int(input('>>'))  # int para converter em numeros inteiros
+print('segundo numero: ')
+segundo_num = int(input('>>'))
+soma = primeiro_num + segundo_num
+print('A soma é: ' + str(soma))  # str para converter em texto
