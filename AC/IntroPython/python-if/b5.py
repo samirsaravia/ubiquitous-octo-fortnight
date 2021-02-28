ed51682f4ba379ef7e0ffc3826c33e829e988829
Samir <samir.saravia.10@gmail.com>
2021-02-28 15:54:07 -0300

    Conhecendo True e False

---
+++ AC/IntroPython/python-if/b5.py
@@ -0,0 +1,23 @@
+# quando é verdadeiro e quando é falso
+print(bool('True'))
+print(bool('False'))
+print(bool(''))
+print(bool(' '))
+print(bool('ola mundo'))
+
+print(bool(7))
+print(bool(1))
+print(bool(0))
+print(bool(-1))
+
+print(1 + 1 == 3)
+print(1 + 1 == 2)
+# print(1 + 1 = 2) existe um erro
+
+
+print(1 == 2)
+print(1 != 4)  # != diferente de
+print(2 < 9)  # menor que
+print(4 > 5)  # maior que
+print(2 >= 3)  # maior ou igual a
+print(5 <= 7)  # menor ou igual a
